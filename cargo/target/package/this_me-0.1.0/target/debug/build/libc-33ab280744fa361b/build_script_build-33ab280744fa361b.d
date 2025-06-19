dd6dad4df0e2bbb23922b246132fc476d998d833
Sui Gn <natsynapses@gmail.com>
2025-06-19 16:26:09 -0500

    cargo and crates

--- cargo/target/package/this_me-0.1.0/target/debug/build/libc-33ab280744fa361b/build_script_build-33ab280744fa361b.d
+++
@@ -1,5 +0,0 @@
-/Users/abellae/Desktop/Neuroverse/all.this/codespaces/this.me/cargo/target/package/this_me-0.1.0/target/debug/build/libc-33ab280744fa361b/build_script_build-33ab280744fa361b: /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.172/build.rs
-
-/Users/abellae/Desktop/Neuroverse/all.this/codespaces/this.me/cargo/target/package/this_me-0.1.0/target/debug/build/libc-33ab280744fa361b/build_script_build-33ab280744fa361b.d: /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.172/build.rs
-
-/Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.172/build.rs:
--- cargo/target/package/this_me-0.1.0/target/debug/deps/unicode_ident-c4cd5a2669b29311.d
+++
@@ -1,8 +0,0 @@
-/Users/abellae/Desktop/Neuroverse/all.this/codespaces/this.me/cargo/target/package/this_me-0.1.0/target/debug/deps/libunicode_ident-c4cd5a2669b29311.rmeta: /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/abellae/Desktop/Neuroverse/all.this/codespaces/this.me/cargo/target/package/this_me-0.1.0/target/debug/deps/libunicode_ident-c4cd5a2669b29311.rlib: /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/abellae/Desktop/Neuroverse/all.this/codespaces/this.me/cargo/target/package/this_me-0.1.0/target/debug/deps/unicode_ident-c4cd5a2669b29311.d: /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
-
-/Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs:
-/Users/abellae/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs:
